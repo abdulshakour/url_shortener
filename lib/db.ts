ed05c310aa4ef264307256a78a54828545e624bb
abdulshakourr <shakirteck4@gmail.com>
2024-09-25 12:40:28 +0300

    test

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-//
-// const db =
-//   global.prisma ||
-//   new PrismaClient({
-//     log: ["query", "info", "warn", "error"], // Enable logging for all actions
-//   });
-// if (process.env.NODE_ENV !== "production") global.prisma = db;
-//
+const db =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // Enable logging for all actions
+  });
+if (process.env.NODE_ENV !== "production") global.prisma = db;
+
 export default db;
